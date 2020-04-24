34a6a84d2f66034f4949aef2afcfb2522d41c84f
Krunal20 <61117791+Krunal20@users.noreply.github.com>
2020-04-24 19:20:09 +0530

    Changed ipynb to py
    For conveniently traceable changes

--- Fake News Detector.py
+++ Fake News Detector.py
@@ -28,14 +28,25 @@ data.label.value_counts()
 
 #Categories are balanced.
 #Defining preprocessing function
+from nltk.corpus import wordnet
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN)
+
 def preprocessing(col):
     #Lower case
     lower = col.apply(str.lower)
 
-    #Stemming
-    from nltk.stem import SnowballStemmer
-    stemmer = SnowballStemmer('english')
-    lemmatized = lower.apply(lambda x: ' '.join(stemmer.stem(word) for word in str(x).split()))
+    #Lemmatizing
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = lower.apply(lambda x: ' '.join(lemmatizer.lemmatize(word,get_wordnet_pos(word)) for word in str(x).split()))
     
     #removing stopwords and extra spaces
     from nltk.corpus import stopwords
@@ -60,10 +71,12 @@ def preprocessing(col):
 
 
 #Processing data for vectorization
-data = pd.concat([pd.get_dummies(data.label).drop('FAKE',axis=1),data.drop(['label','Unnamed: 0'],axis=1)],axis=1)
-data['xdata'] = data.title + " " + data.text
-data = data.drop(['title', 'text'], axis = 1)
-data['xdata'] = preprocessing(data['xdata'])
+#news_processed was processed using the commands given below
+#data = pd.concat([pd.get_dummies(data.label).drop('FAKE',axis=1),data.drop(['label','Unnamed: 0'],axis=1)],axis=1)
+#data['xdata'] = data.title + " " + data.text
+#data = data.drop(['title', 'text'], axis = 1)
+#data['xdata'] = preprocessing(data['xdata'])
+data = pd.read_csv('G:/Users/Krunal/Desktop/news_processed.csv')
 
 
 
@@ -71,7 +84,7 @@ data['xdata'] = preprocessing(data['xdata'])
 #splitting data into training and testing sets of x and y
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-tf = TfidfVectorizer(min_df = 5)
+tf = TfidfVectorizer(ngram_range = (1,3), min_df = 5)
 xset = data.xdata
 yset = data.REAL
 xtrain, xtest, ytrain, ytest = train_test_split(xset, yset, test_size=0.33, random_state = 123)
@@ -89,8 +102,6 @@ xtesttf = tf.transform(xtest.values.astype(str))
 #initializing and training model
 from sklearn.linear_model import PassiveAggressiveClassifier
 pac = PassiveAggressiveClassifier(random_state = 79).fit(xtraintf,ytrain)
-ypred = pac.predict(xtesttf)
-dict_accu.update({r : np.mean(ypred == ytest)*100})
 
 
 
